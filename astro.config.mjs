94c4b40c49afea4389f70f90babed03d09df16fe
DominicDev <s0l0estudi4r@gmail.com>
2025-06-22 19:31:18 +0200

    v0.0.1

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  build: {
+    assets: 'static'
+  },
+
+  //Deploy normal project
+  // base: '/project-000-948/',
+  // site: 'https://20essentials.github.io/project-000-948/'
+
+  //Deploy 20 Projects
+  // base: '/20-projects-with-nunjucks/',
+  // site: 'https://20essentials.github.io/20-projects-with-nunjucks/'
+
+  //Localhost
+  site: 'http://localhost:4321/',
+
+  integrations: [react()]
+});
---
+++ src/components/Phone.tsx
@@ -0,0 +1,5 @@
+import '@/styles/phone.css'; 
+
+export function Phone() {
+  return <main className='phone'></main>;
+}
