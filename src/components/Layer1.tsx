61728bfcd8e771134bc705674de3693e9e678da2
DominicDev <s0l0estudi4r@gmail.com>
2025-06-22 20:32:09 +0200

    v0.0.5

--- src/components/Layer1.tsx
+++ src/components/Layer1.tsx
@@ -1,5 +1,15 @@
 import '@/styles/Layer1.css';
 
 export function Layer1() {
-  return <article className='layer1 layer-container'></article>;
+  return <article className='layer1 layer-container'>
+    <aside className="padTop"></aside>
+    <section className="container-grid">
+      {
+        [...new Array(35).keys()].map((n) => <aside className="social-container">
+          <article className="social-top"></article>
+          <article className="social-bottom">Tik Tok</article>
+        </aside>)
+      }
+    </section>
+  </article>;
 }
---
+++ src/components/Layer2.tsx
@@ -0,0 +1,5 @@
+import '@/styles/Layer2.css';
+
+export function Layer2() {
+  return <article className='layer2 layer-container'></article>;
+}
