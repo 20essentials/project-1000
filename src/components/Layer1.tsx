3a1d9e5409d4a37ca207e75ab95bcbfb521a43f4
DominicDev <s0l0estudi4r@gmail.com>
2025-06-22 20:48:02 +0200

    v0.0.6

--- src/components/Layer1.tsx
+++ src/components/Layer1.tsx
@@ -1,14 +1,21 @@
 import '@/styles/Layer1.css';
+import { useState } from 'react';
 
 export function Layer1() {
+  const [indexOfVisibleIcon, setindexOfVisibleIcon] = useState(12);
+
   return <article className='layer1 layer-container'>
     <aside className="padTop"></aside>
     <section className="container-grid">
       {
-        [...new Array(35).keys()].map((n) => <aside className="social-container">
+        [...new Array(35).keys()].map((idx) => {
+          const isVisible = idx === indexOfVisibleIcon;
+          const className = `social-container ${isVisible ? 'visible' : ''}`
+          return <aside key={idx} className={className}>
           <article className="social-top"></article>
-          <article className="social-bottom">Tik Tok</article>
-        </aside>)
+          <article className="social-bottom">TikTok</article>
+        </aside>
+        })
       }
     </section>
   </article>;
