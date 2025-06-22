b12256eb1b7ee76bf6fca02106a07851fc1377e3
DominicDev <s0l0estudi4r@gmail.com>
2025-06-22 19:49:40 +0200

    v0.0.3

---
+++ src/components/Layer0.tsx
@@ -0,0 +1,5 @@
+import '@/styles/Layer0.css'; 
+
+export function Layer0() {
+  return <article className='layer0'></article>;
+}
--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -1,5 +1,10 @@
 import '@/styles/phone.css'; 
+import { Layer1 } from './Layer1';
+import { Layer0 } from '@/components/Layer0';
 
 export function Phone() {
-  return <main className='phone'></main>;
+  return <main className='phone'>
+    {/* <Layer1></Layer1> */}
+    <Layer0 />
+  </main>;
 }
