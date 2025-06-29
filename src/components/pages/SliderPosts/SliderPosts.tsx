456ea0e8d8d78606b7812ba534214cdcd445c1e3
DominicDev <s0l0estudi4r@gmail.com>
2025-06-29 23:04:41 +0200

    v0.3.9

--- src/components/pages/SliderPosts/SliderPosts.tsx
+++ src/components/pages/SliderPosts/SliderPosts.tsx
@@ -7,26 +7,28 @@ import { PRIVATE_DATA } from '@/privateData/amPrivateData';
 import { PUBLIC_DATA } from '@/publicData/amPublicData';
 import { useEffect, useRef } from 'react';
 import { useLimitOfPost } from '@/store/useLimitOfPosts';
+import { useMemo } from 'react';
 
 const FOLLOWED: arrayOfPosts = [...PRIVATE_DATA];
 const FOR_YOU: arrayOfPosts = [...PUBLIC_DATA];
+
 
 export function SliderPosts() {
   const isForYou = useFollowedOrForYou(state => state.isForYou);
   const ALL_POSTS = isForYou ? FOR_YOU : FOLLOWED;
   const sliderRef = useRef<HTMLDivElement>(null);
   const limit = useLimitOfPost(state => state.limit);
-  
+  const flattenedPosts = useMemo(() => {
+  return ALL_POSTS.flatMap(([userCommonProps, userPosts]) =>
+    userPosts.map(post => ({ ...post, ...userCommonProps }))
+  ).toSorted(() => Math.random() - 0.5);
+}, [ALL_POSTS]);
 
   useEffect(() => {
     if (sliderRef.current) {
       sliderRef.current.scrollTop = 0;
     }
   }, [isForYou]);
-
-  const flattenedPosts = ALL_POSTS.flatMap(([userCommonProps, userPosts]) =>
-    userPosts.map(post => ({ ...post, ...userCommonProps }))
-  );
 
   const postsToShow = flattenedPosts.slice(0, limit);
 
