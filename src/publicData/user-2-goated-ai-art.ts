0a25934ea6fd17044832b828c2cbdbf638de2656
DominicDev <s0l0estudi4r@gmail.com>
2025-06-30 23:09:22 +0200

    v0.4.3

--- src/publicData/user-2-goated-ai-art.ts
+++ src/publicData/user-2-goated-ai-art.ts
@@ -16,13 +16,413 @@ export const goated_ai_art_commonProps = generateExactlyCommonProps({
 });
 
 const PREFIX = '';
-
 const ARRAY_CONTENT: ArrayContent = [
   {
-    type: 'image',
-    arrayImages: ['f1.avif']
+    type: "image",
+    arrayImages: ["f1.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f2.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f3.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f4.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f5.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f6.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f7.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f8.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f9.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f10.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f11.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f12.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f13.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f14.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f15.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f16.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f17.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f18.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f19.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f20.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f21.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f22.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f23.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f24.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f25.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f26.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f27.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f28.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f29.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f30.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f31.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f32.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f33.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f34.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f35.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f36.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f37.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f38.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f39.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f40.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f41.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f42.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f43.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f44.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f45.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f46.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f47.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f48.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f49.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f50.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f51.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f52.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f53.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f54.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f55.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f56.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f57.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f58.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f59.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f60.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f61.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f62.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f63.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f64.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f65.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f66.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f67.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f68.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f69.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f70.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f71.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f72.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f73.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f74.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f75.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f76.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f77.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f78.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f79.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f80.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f81.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f82.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f83.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f84.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f85.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f86.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f87.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f88.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f89.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f90.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f91.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f92.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f93.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f94.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f95.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f96.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f97.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f98.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f99.avif"]
+  },
+  {
+    type: "image",
+    arrayImages: ["f100.avif"]
+  },
+  {
+    type: "video",
+    videoSrc: "v1.mp4"
   }
 ];
+
 
 export const goated_ai_art_array_of_posts = generateArrayPosts({
   PREFIX,
