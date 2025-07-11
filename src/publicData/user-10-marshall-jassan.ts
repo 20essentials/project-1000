6fc6378e3d3b078905b9ea5bc7da033fc0f13019
DominicDev <s0l0estudi4r@gmail.com>
2025-07-11 17:59:11 +0200

    v0.8.4

--- src/publicData/user-10-marshall-jassan.ts
+++ src/publicData/user-10-marshall-jassan.ts
@@ -9,9 +9,10 @@ import {
 */
 
 export const marshall_jassan_commonProps = generateExactlyCommonProps({
-  userId: '',
+  userId: '83cdb1f4-dee1-462b-bfe1-617be369a910',
   username: 'marshall_jassan',
-  profileImageSrc: '',
+  profileImageSrc:
+    'https://20essentials.github.io/project-000-940/assets/j3.avif',
   following: false,
   followers: 6198,
   totalLikes: 147_300,
@@ -19,69 +20,89 @@ export const marshall_jassan_commonProps = generateExactlyCommonProps({
   profileDescription: 'Hola, hablo sobre ciencia en videos 🪐'
 });
 
-const PREFIX = '';
+const PREFIX =
+  'https://dominicnikolai.github.io/project-196/assets/m4rshall-j4ssan/';
 
 const ARRAY_CONTENT: ArrayContent = [
   {
     type: 'video',
     videoSrc: 'v1.mp4',
     tags: ['aerospace', 'science'],
-    description: `Hola, hablo sobre ciencia en videos 🪐`
+    description: `Hola, hablo sobre ciencia en videos 🪐`,
+    isLiked: false,
+    idPost: `${marshall_jassan_commonProps.userId}-4a7a3a5f-c3f1-43ef-9f1b-c3a4df9e8f45`
   },
   {
     type: 'video',
     videoSrc: 'v2.mp4',
     tags: ['SpaceX', 'Rockets'],
-    description: `SpaceX Launch 🚀`
+    description: `SpaceX Launch 🚀`,
+    isLiked: false,
+    idPost: `${marshall_jassan_commonProps.userId}-aabf31c8-6dd3-4dc5-9e30-2a9307f15b60`
   },
   {
     type: 'video',
     videoSrc: 'v3.mp4',
     tags: ['space', 'jameswebb'],
-    description: `SMACS 0723 por el James Webb HAA ES HERMOSO ya quiero ver las demas mañana 💫`
+    description: `SMACS 0723 por el James Webb HAA ES HERMOSO ya quiero ver las demas mañana 💫`,
+    idPost: `${marshall_jassan_commonProps.userId}-1954a8c9-5b6c-404b-a9a3-420486e50dbe`,
+    isLiked: false
   },
   {
     type: 'video',
     videoSrc: 'v4.mp4',
-    description: `PARTES DE UN COHETE SONDA 🚀 `
-    
+    description: `PARTES DE UN COHETE SONDA 🚀 `,
+    idPost: `${marshall_jassan_commonProps.userId}-5ce3fcde-e0fd-44b4-93f6-6a6b880b16b1`,
+    isLiked: false
   },
   {
     type: 'video',
     videoSrc: 'v5.mp4',
     tags: ['aviation', 'aerospace'],
-    description: `Sistemas de control primarios ✈️  `
+    description: `Sistemas de control primarios ✈️  `,
+    idPost: `${marshall_jassan_commonProps.userId}-93b3a3a1-1d63-41ef-800f-01cb508376c4`,
+    isLiked: false
   },
   {
     type: 'video',
     videoSrc: 'v6.mp4',
     tags: ['telescopio', 'moon'],
-    description: `Ya has visto antes por telescopio? Qué te gusta o gustaría observar?`
+    description: `Ya has visto antes por telescopio? Qué te gusta o gustaría observar?`,
+    idPost: `${marshall_jassan_commonProps.userId}-63ad3ea9-c361-4f88-b7b3-0a2e139d4231`,
+    isLiked: false
   },
   {
     type: 'video',
     videoSrc: 'v7.mp4',
     tags: ['rocket', 'cohete'],
-    description: `This is so cool!! 🚀 `
+    description: `This is so cool!! 🚀 `,
+    idPost: `${marshall_jassan_commonProps.userId}-d1ce50b6-79cb-4ef8-89f6-722ff06237c3`,
+    isLiked: false
   },
   {
     type: 'video',
     videoSrc: 'v8.mp4',
     tags: ['astronaut', 'space'],
-    description: `Me quedé con ganas de ponermelo y llevármelo a mi casa 😬`
+    description: `Me quedé con ganas de ponermelo y llevármelo a mi casa 😬`,
+    idPost: `${marshall_jassan_commonProps.userId}-d3f6df3d-12b1-4e8e-902a-2a22bc5d5432`,
+    isLiked: false
   },
   {
     type: 'video',
     videoSrc: 'v9.mp4',
     tags: ['rocket', 'cool'],
-    description: `Comenzaré a subir estos proyectos por aquí 🚀`
+    description: `Comenzaré a subir estos proyectos por aquí 🚀`,
+    idPost: `${marshall_jassan_commonProps.userId}-e51907e6-3e7f-4f76-b62f-cf8611f86f70`,
+    isLiked: false
   },
   {
     type: 'video',
     videoSrc: 'v10.mp4',
     tags: ['aerospace', 'carlsagan'],
-    description: `The Little Voyager ya tiene marcas de guerra 🚀`
-  },
+    description: `The Little Voyager ya tiene marcas de guerra 🚀`,
+    idPost: `${marshall_jassan_commonProps.userId}-2fd2bdf2-4732-45fc-90f1-7e78867c2a56`,
+    isLiked: false
+  }
 ];
 
 export const marshall_jassan_array_of_posts = generateArrayPosts({
---
+++ src/publicData/user-12-jorgepomarg.ts
@@ -0,0 +1,94 @@
+import {
+  generateExactlyCommonProps,
+  generateArrayPosts,
+  type ArrayContent
+} from '@/publicData/functions/amPublicFunctions';
+
+/*
+  falsta: userid, profilesrc, prefix
+*/
+
+export const jorgepomarg_commonProps = generateExactlyCommonProps({
+  userId: '',
+  username: 'jorgepomarg',
+  profileImageSrc: '',
+  following: false,
+  followers: 15_200,
+  totalLikes: 639_720,
+  followed: 109,
+  profileDescription: '😅'
+});
+
+const PREFIX = '';
+
+const ARRAY_CONTENT: ArrayContent = [
+  {
+    type: 'video',
+    videoSrc: 'v1.mp4',
+    tags: ['senate', 'ai', 'future'],
+    description: ` El día de ayer Elon Musk, Bill Gates, Mark Zuckerberg y multitud de gigantes tecnológicos se reunieron a puerta cerrada con el Senado de Estados Unidos para discutir el futuro de la Inteligencia Artificial y su regulación`,
+    isLiked: false,
+    idPost: `${jorgepomarg_commonProps.userId}-9fd97a84-c39d-4c2e-872f-53eb5fbdfe4e`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v2.mp4',
+    tags: ['cripto', 'bitcoin', "fyp"],
+    description: `🚁🚁🚁`,
+    isLiked: false,
+    idPost: `${jorgepomarg_commonProps.userId}-2a04dc1e-8c3e-4950-92d2-470487e3f8e1`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v3.mp4',
+    tags: ['profit', 'gananacia'],
+    description: `✨🌴`,
+    isLiked: false,
+    idPost: `${jorgepomarg_commonProps.userId}-cfba6620-d11c-4baf-8546-91d426127ede`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v4.mp4',
+    tags: ['nft', 'simpson'],
+    description: `En el último episodio de Los Simpson Convierten a Bart en un NFT, presentándole crypto a muchas personas.`,
+    isLiked: false,
+    idPost: `${jorgepomarg_commonProps.userId}-d59c3b2f-f3b3-4df0-bd30-ec9c11d27f30`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v5.mp4',
+    tags: ['eclipse', 'anillo de fuego'],
+    description: `Así luce el efecto "Anillo de Fuego", provocado durante el eclipse anular de Sol, sobre Nevada, en Estados Unidos.`,
+    isLiked: false,
+    idPost: `${jorgepomarg_commonProps.userId}-e706089d-69a2-4f89-85c3-70912b2cfc84`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v6.mp4',
+    tags: ['rayban', 'meta'],
+    description: `Probaron los nuevos Ray-Ban de Meta, que pueden conectarse y transmitir directamente a Instagram.`,
+    isLiked: false,
+    idPost: `${jorgepomarg_commonProps.userId}-3e9b2094-ffb3-4cf4-aebc-7db3a9e8b9e4`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v7.mp4',
+    tags: ['tormenta', 'tornado'],
+    description: `Tres cazadores de tormentas encuentran un tornado y no pueden escapar a tiempo.`,
+    isLiked: false,
+    idPost: `${jorgepomarg_commonProps.userId}-6bcd548d-6371-409b-b7c9-63ee02aae38f`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v8.mp4',
+    tags: ['tullipan', 'mineria'],
+    description: `El calor generado en la minería de bitcoins se utiliza en el cultivo de tulipanes en los Países Bajos.`,
+    isLiked: false,
+    idPost: `${jorgepomarg_commonProps.userId}-6d0e1d04-2dac-4f7f-8e47-4c1ecc61d614`
+  },
+];
+
+export const jorgepomarg_array_of_posts = generateArrayPosts({
+  PREFIX,
+  ARRAY_CONTENT
+});
