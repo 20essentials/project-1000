1fad30008217994a1daaacfc8cf32640c28a0b66
DominicDev <s0l0estudi4r@gmail.com>
2025-07-29 17:54:43 +0200

    v1.3.5

---
+++ src/publicData/user-38-salary-transparent-street.ts
@@ -0,0 +1,147 @@
+import {
+  generateExactlyCommonProps,
+  generateArrayPosts,
+  type ArrayContent
+} from '@/publicData/functions/amPublicFunctions';
+
+//falta userId, profilesrc, prefix, profileDescription
+
+export const salary_transparent_street_commonProps = generateExactlyCommonProps({
+  userId: '',
+  username: 'salary_transparent_street',
+  profileImageSrc: '',
+  following: false,
+  followers: 1_500_720,
+  followed: 50,
+  profileDescription: 'How much do you make?'
+});
+
+const PREFIX = '';
+
+const ARRAY_CONTENT: ArrayContent = [
+  {
+    type: 'video',
+    videoSrc: 'v1.mp4',
+    description: `Old Town, Alexandria, VA📍Tell us which cities we should hit on our nationwide tour!👇 #salarytransparency #salarytransparentstreet #paytransparency #equalpay #closethepaygap #diversityandinclusion`,
+    idPost: `${salary_transparent_street_commonProps.userId}-9fd97a84-c39d-4c2e-872f-53eb5fbdfe4e`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v2.mp4',
+    description: `#CloudEngineers in Atlanta, GA📍#salarytransparentstreet #salarytransparency #paytransparency #howmuchdoyoumake #cloudengineer #cloudengineersalary #techjob #techsalary #engineerjob`,
+    idPost: `${salary_transparent_street_commonProps.userId}-2a04dc1e-8c3e-4950-92d2-470487e3f8e1`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v3.mp4',
+    description: `Philadelphia, PA📍(with a wholesome sprinkle from DC 🌸) #salarytransparency #salarytransparentstreet #paytransparency #equalpay #closethewagegap #diversityandinclusion`,
+    idPost: `${salary_transparent_street_commonProps.userId}-cfba6620-d11c-4baf-8546-91d426127ede`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v4.mp4',
+    description: `#travelnurse based in California📍#salarytransparentstreet #salarytransparent #paytransparency #equalpay #diversityandinclusion #dei #talkaboutmoney #travelnursepay #travelnursesalary #healthcarejob #nurse #healthcare`,
+    idPost: `${salary_transparent_street_commonProps.userId}-d59c3b2f-f3b3-4df0-bd30-ec9c11d27f30`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v5.mp4',
+    description: `How much do Software Engineers make? #salarytransparentstreet compilation!`,
+    idPost: `${salary_transparent_street_commonProps.userId}-e706089d-69a2-4f89-85c3-70912b2cfc84`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v6.mp4',
+    description: `#Chiropractor and #CardiovascularPerfusionist in Portland, OR📍#salarytransparentstreet #salarytransparency #paytransparency #howmuchdoyoumake #talkaboutyourpay #chiropractorjob #chiropractorsalary`,
+    idPost: `${salary_transparent_street_commonProps.userId}-3e9b2094-ffb3-4cf4-aebc-7db3a9e8b9e4`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v7.mp4',
+    description: `#CyberSecurityAnalyst making $150k in Washington, DC📍!`,
+    idPost: `${salary_transparent_street_commonProps.userId}-6bcd548d-6371-409b-b7c9-63ee02aae38f`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v8.mp4',
+    description: `#DataScientist in Denver, CO📍#salarytransparentstreet #salarytransparency #paytransparency #howmuchdoyoumake #talkaboutyourpay #techjob #datascientistjob #datascientistsalary`,
+    idPost: `${salary_transparent_street_commonProps.userId}-6d0e1d04-2dac-4f7f-8e47-4c1ecc61d614`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v9.mp4',
+    description: `#SoftwareEngineer in Seattle, WA📍#salarytransparentstreet #salarytransparency #paytransparency #howmuchdoyoumake #softwareengineersalary #techjob #techsalary`,
+    idPost: `${salary_transparent_street_commonProps.userId}-397c28c9-7cfc-4503-a6e1-fa7b20ce0eef`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v10.mp4',
+    description: `The Wharf, Washington, D.C.📍#salarytransparentstreet #salarytransparency #paytransparency #equalpay #closethepaygap #talkaboutyourpay #diversityandinclusion #thewharf #washingtondc`,
+    idPost: `${salary_transparent_street_commonProps.userId}-613257bd-2429-489a-9694-4e4e68827333`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v11.mp4',
+    description: `Thanks to teamblind for helping us showcase different tech salaries in San Francisco, CA! If you'd like to make sure you're being paid fairly, be sure to use Blind's Total Compensation Tool at the link in our Stan store. #TeamBlindPartner #salarytransparentstreet #salarytransparency #paytransparency #techindustry #techsalaries #salariesintech #techpay #techsalary #howmuchdoyoumake`,
+    idPost: `${salary_transparent_street_commonProps.userId}-276da371-8abb-41f5-ba4a-b5042786c748`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v12.mp4',
+    description: `How much does a Senior Federal Technical #AccountManager make in Washington, DC📍? Watch the full interview on YouTube to learn which cybersecurity certifications she recommends, how to increase your salary internally by job-hopping, and how to get a federal job without a security clearance. If you have any questions, reach out to Symoné B. | GovTech! #salarytransparentstreet #salarytransparency #technicalaccountmanager #techjob #federaltechjob #cybersecurityjob #comptia`,
+    idPost: `${salary_transparent_street_commonProps.userId}-b467d312-cb8d-4554-bec0-590590565d77`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v13.mp4',
+    description: ``,
+    idPost: `${salary_transparent_street_commonProps.userId}-a6a20e67-0771-4112-a915-d4db596daef6`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v14.mp4',
+    description: ``,
+    idPost: `${salary_transparent_street_commonProps.userId}-43973d48-d59b-4dc7-aaa1-c5e69ea055f7`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v15.mp4',
+    description: ``,
+    idPost: `${salary_transparent_street_commonProps.userId}-9f278864-6362-4e81-a406-fc99443828a1`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v16.mp4',
+    description: ``,
+    idPost: `${salary_transparent_street_commonProps.userId}-e8023a99-ae84-46f0-9f6f-133d9cf5b741`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v17.mp4',
+    description: ``,
+    idPost: `${salary_transparent_street_commonProps.userId}-53b20bf6-9713-4530-aee7-e0b96ba95280`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v18.mp4',
+    description: ``,
+    idPost: `${salary_transparent_street_commonProps.userId}-27c1569a-7950-48dc-9384-079e46448ba5`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v19.mp4',
+    description: ``,
+    idPost: `${salary_transparent_street_commonProps.userId}-8d9b86b2-4f3c-46b3-be0c-4157fecc9ae6`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v20.mp4',
+    description: ``,
+    idPost: `${salary_transparent_street_commonProps.userId}-b8dee4fb-3715-4c10-a916-419d81ea91f4`
+  },
+];
+
+export const salary_transparent_street_array_of_posts = generateArrayPosts({
+  PREFIX,
+  ARRAY_CONTENT
+});
