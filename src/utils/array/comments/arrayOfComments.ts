488b8aa8a21dbcb2dbc86b7e5e9e0f9364d9d2ae
DominicDev <s0l0estudi4r@gmail.com>
2025-08-02 00:32:13 +0200

    v1.7.0

--- src/utils/array/comments/arrayOfComments.ts
+++ src/utils/array/comments/arrayOfComments.ts
@@ -1,16 +1,16 @@
 import { EMOJIS } from '@/utils/array/comments/arrayOfEmojis';
 import { EMOJIS_REPEATED } from '@/utils/array/comments/arrayOfRepeatedEmojis';
 import { JAJAJAJS } from '@/utils/array/comments/arrayOfJajajaj';
-import { EPICO } from './arrayOfEpic';
-import { BRUTAL } from './arrayofBrutal';
-import { TOP } from './arrayofTop';
-import { ACUERDO } from './arrayofAcuerdo';
-import { ARTE } from './arrayofArte';
-import { NECESITAR } from './arrayOfComment';
-import { FIRMAR } from './arrayofFirmar';
-import { GOOD_CONTENT } from './arrayofGoodContent';
-import { AFIRMAR } from './arrayofAfirmar';
-import { AGRADECER } from './arrayofAgradecimiento';
+import { EPICO } from '@/utils/array/comments/arrayOfEpic';
+import { BRUTAL } from '@/utils/array/comments/arrayofBrutal';
+import { TOP } from '@/utils/array/comments/arrayofTop';
+import { ACUERDO } from '@/utils/array/comments/arrayofAcuerdo';
+import { ARTE } from '@/utils/array/comments/arrayofArte';
+import { NECESITAR } from '@/utils/array/comments/arrayOfComment';
+import { FIRMAR } from '@/utils/array/comments/arrayofFirmar';
+import { GOOD_CONTENT } from '@/utils/array/comments/arrayofGoodContent';
+import { AFIRMAR } from '@/utils/array/comments/arrayofAfirmar';
+import { AGRADECER } from '@/utils/array/comments/arrayofAgradecimiento';
 
 export const ARRAY_OF_COMMENTS = [
   ...EMOJIS,
