de1a189c33771f1356c97143526606e90ff102d2
DominicDev <s0l0estudi4r@gmail.com>
2025-08-03 13:54:37 +0200

    v1.9.2

--- src/components/pages/SliderPosts/AsideRight/AsideBottomOfShare.tsx
+++ src/components/pages/SliderPosts/AsideRight/AsideBottomOfShare.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { CloseContainer } from './CloseContainer';
 
 const ARRAY_OF_SHARE = [
@@ -12,7 +13,44 @@ const ARRAY_OF_SHARE = [
           d='m12 16l-5-5l1.4-1.45l2.6 2.6V4h2v8.15l2.6-2.6L17 11zm-6 4q-.825 0-1.412-.587T4 18v-3h2v3h12v-3h2v3q0 .825-.587 1.413T18 20z'
         />
       </svg>
-    )
+    ),
+    onclick: ({
+      url,
+      nombreArchivo,
+      refButton
+    }: {
+      url: string;
+      nombreArchivo: string;
+      refButton: React.RefObject<HTMLButtonElement | null>;
+    }) => {
+      const $buttonSystem = refButton?.current;
+      if ($buttonSystem instanceof HTMLButtonElement) {
+        $buttonSystem.classList.add('ssd');
+        $buttonSystem.innerHTML = 'Downloading';
+      }
+
+      fetch(url)
+        .then(response => {
+          if (!response.ok) {
+            console.log('The File cannot be download');
+            return;
+          }
+          return response.blob();
+        })
+        .then(blob => {
+          const blobUrl = URL.createObjectURL(blob as Blob);
+          const a = document.createElement('a');
+          a.href = blobUrl;
+          a.download = nombreArchivo;
+          document.body.appendChild(a);
+          a.click();
+          a.remove();
+          URL.revokeObjectURL(blobUrl);
+        })
+        .finally(() => {
+          refButton.current && (refButton.current.innerHTML = 'Downloading');
+        });
+    }
   },
   {
     title: 'Copy Link',
@@ -225,10 +263,7 @@ const ARRAY_OF_SHARE = [
     id: 'd1e2f3g4-5h6i-7j8k-9l0m-1n2o3p4q5r6u',
     gradient: 'linear-gradient(135deg, #7289da, #5b6eae)',
     svg: (
-      <svg
-        xmlns='http://www.w3.org/2000/svg'
-        viewBox='0 0 24 24'
-      >
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
         <path
           fill='currentColor'
           d='M19.27 5.33C17.94 4.71 16.5 4.26 15 4a.1.1 0 0 0-.07.03c-.18.33-.39.76-.53 1.09a16.1 16.1 0 0 0-4.8 0c-.14-.34-.35-.76-.54-1.09c-.01-.02-.04-.03-.07-.03c-1.5.26-2.93.71-4.27 1.33c-.01 0-.02.01-.03.02c-2.72 4.07-3.47 8.03-3.1 11.95c0 .02.01.04.03.05c1.8 1.32 3.53 2.12 5.24 2.65c.03.01.06 0 .07-.02c.4-.55.76-1.13 1.07-1.74c.02-.04 0-.08-.04-.09c-.57-.22-1.11-.48-1.64-.78c-.04-.02-.04-.08-.01-.11c.11-.08.22-.17.33-.25c.02-.02.05-.02.07-.01c3.44 1.57 7.15 1.57 10.55 0c.02-.01.05-.01.07.01c.11.09.22.17.33.26c.04.03.04.09-.01.11c-.52.31-1.07.56-1.64.78c-.04.01-.05.06-.04.09c.32.61.68 1.19 1.07 1.74c.03.01.06.02.09.01c1.72-.53 3.45-1.33 5.25-2.65c.02-.01.03-.03.03-.05c.44-4.53-.73-8.46-3.1-11.95c-.01-.01-.02-.02-.04-.02M8.52 14.91c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.84 2.12-1.89 2.12m6.97 0c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.83 2.12-1.89 2.12'
@@ -284,6 +319,8 @@ export function AsideBottomOfShare({
 }: {
   updateIsContainerShareOpen: () => void;
 }) {
+  const systembtn = useRef<null | HTMLButtonElement>(null);
+
   return (
     <article className='aside-bottom-of-share'>
       <header className='am-header-of-share'>
@@ -292,6 +329,11 @@ export function AsideBottomOfShare({
           className='close-container-bottom'
           onClick={updateIsContainerShareOpen}
         />
+        <div className='system-container'>
+          <button className='system-btn' ref={systembtn}>
+            OK
+          </button>
+        </div>
       </header>
       <section className='am-footer-of-the-share'>
         {ARRAY_OF_SHARE.map(({ id, title, gradient, svg }) => {
---
+++ src/publicData/user-48-ligth-speed.ts
@@ -0,0 +1,81 @@
+import {
+  generateExactlyCommonProps,
+  generateArrayPosts,
+  type ArrayContent
+} from '@/publicData/functions/amPublicFunctions';
+
+//falta userId, profilesrc, prefix, profileDescription
+
+export const ligthspeed_commonProps = generateExactlyCommonProps({
+  userId: '',
+  username: 'ligthspeed',
+  profileImageSrc: '',
+  following: false,
+  followers: 255_000,
+  followed: 132,
+  profileDescription: 'Stage 2 Ninja H2 GSXR-1000'
+});
+
+const PREFIX = '';
+
+const ARRAY_CONTENT: ArrayContent = [
+  {
+    type: 'video',
+    videoSrc: 'v1.mp4',
+    description: `Ultimate dream bike 😍🙌🏼 #Motorcycle #biker #BikeLife #superbike #sportbike #supersport #hypersport #Kawasaki⁣⁣⁣⁣⁣⁣ #H2 #H2R #ninjah2`,
+    idPost: `${ligthspeed_commonProps.userId}-9fd97a84-c39d-4c2e-872f-53eb5fbdfe4e`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v2.mp4',
+    description: `Perfect lines and curves 😍😮‍💨 #Motorcycle #biker #BikeLife #superbike #sportbike #supersport #hypersport #Kawasaki⁣⁣⁣⁣⁣⁣ #H2 #H2R #ninjah2`,
+    idPost: `${ligthspeed_commonProps.userId}-2a04dc1e-8c3e-4950-92d2-470487e3f8e1`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v3.mp4',
+    description: `The pilot about to do a top speed run! 🛩️😈🔥 #Motorcycle #biker #BikeLife #superbike #sportbike #supersport #hypersport #Kawasaki⁣⁣⁣⁣⁣⁣ #H2 #H2R #ninjah2`,
+    idPost: `${ligthspeed_commonProps.userId}-cfba6620-d11c-4baf-8546-91d426127ede`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v4.mp4',
+    description: `The vibes are immaculate 😮‍💨👌🏼 #Motorcycle #biker #BikeLife #superbike #sportbike #supersport #hypersport #Kawasaki⁣⁣⁣⁣⁣⁣ #H2 #H2R #ninjah2`,
+    idPost: `${ligthspeed_commonProps.userId}-d59c3b2f-f3b3-4df0-bd30-ec9c11d27f30`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v5.mp4',
+    description: `Night time menace 😈🔥 #Motorcycle #biker #BikeLife #superbike #sportbike #supersport #hypersport #Kawasaki⁣⁣⁣⁣⁣⁣ #H2 #H2R #ninjah2`,
+    idPost: `${ligthspeed_commonProps.userId}-e706089d-69a2-4f89-85c3-70912b2cfc84`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v6.mp4',
+    description: `Paint like a perfect mirror 😮‍💨👌🏼 #Motorcycle #biker #BikeLife #superbike #sportbike #supersport #hypersport #Kawasaki⁣⁣⁣⁣⁣⁣ #H2 #H2R #ninjah2`,
+    idPost: `${ligthspeed_commonProps.userId}-3e9b2094-ffb3-4cf4-aebc-7db3a9e8b9e4`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v7.mp4',
+    description: `#superbike #sportbike #supersport`,
+    idPost: `${ligthspeed_commonProps.userId}-6bcd548d-6371-409b-b7c9-63ee02aae38f`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v8.mp4',
+    description: `#Motorcycle #biker #BikeLife`,
+    idPost: `${ligthspeed_commonProps.userId}-6d0e1d04-2dac-4f7f-8e47-4c1ecc61d614`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v9.mp4',
+    description: `If we being real 😈👑 🎥  #hypersport #Kawasaki⁣⁣⁣⁣⁣⁣ #H2 #H2R #ninjah2`,
+    idPost: `${ligthspeed_commonProps.userId}-397c28c9-7cfc-4503-a6e1-fa7b20ce0eef`
+  }
+];
+
+export const ligthspeed_array_of_posts = generateArrayPosts({
+  PREFIX,
+  ARRAY_CONTENT
+});
