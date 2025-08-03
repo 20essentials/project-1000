d2d21b6b085d49ac16e2982a8ea18f45355dee03
DominicDev <s0l0estudi4r@gmail.com>
2025-08-03 15:31:39 +0200

    v1.9.3

---
+++ src/publicData/user-49-blackcats.ts
@@ -0,0 +1,244 @@
+import {
+  generateExactlyCommonProps,
+  generateArrayPosts,
+  type ArrayContent
+} from '@/publicData/functions/amPublicFunctions';
+
+//falta userId, profilesrc, prefix, profileDescription
+
+export const blackcats_commonProps = generateExactlyCommonProps({
+  userId: '',
+  username: 'blackcats',
+  profileImageSrc: '',
+  following: false,
+  followers: 9_720_520,
+  followed: 150,
+  profileDescription: ''
+});
+
+const PREFIX = '';
+
+const ARRAY_CONTENT: ArrayContent = [
+  {
+    type: 'video',
+    videoSrc: 'v1.mp4',
+    description: `I definitely recommend trying it 🖤 #blackcat #blackcatsrule #blackcats #kittensdaily`,
+    idPost: `${blackcats_commonProps.userId}-9fd97a84-c39d-4c2e-872f-53eb5fbdfe4e`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v2.mp4',
+    description: `Tells me everything I need to know about you! #blackcat #blackcats #blackkitty`,
+    idPost: `${blackcats_commonProps.userId}-2a04dc1e-8c3e-4950-92d2-470487e3f8e1`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v3.mp4',
+    description: `They are more than just a part to me � #blackcats #blackcat #blackcatsrule #soulcat`,
+    idPost: `${blackcats_commonProps.userId}-cfba6620-d11c-4baf-8546-91d426127ede`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v4.mp4',
+    description: `I believe it 🥺🥺 #blackcat #blackcats #blackcatsofig`,
+    idPost: `${blackcats_commonProps.userId}-d59c3b2f-f3b3-4df0-bd30-ec9c11d27f30`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v5.mp4',
+    description: `That would be so neat 😂 #blackcat #blackcats #blackkitten #kitty`,
+    idPost: `${blackcats_commonProps.userId}-e706089d-69a2-4f89-85c3-70912b2cfc84`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v6.mp4',
+    description: `Drama king 👑🐈‍⬛ #tizon #pelusa #iradelosmiaus #gorditosabroson #parati #cat #gatos`,
+    idPost: `${blackcats_commonProps.userId}-3e9b2094-ffb3-4cf4-aebc-7db3a9e8b9e4`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v7.mp4',
+    description: `Un día en la vida tan dura de Tizón, el gordito sabrosón #tizoncito #blacktizon #iradelosmiaus #gorditosabroson #blackcats #catsofinstagram #gatos #gatosnegros #viral #parati`,
+    idPost: `${blackcats_commonProps.userId}-6bcd548d-6371-409b-b7c9-63ee02aae38f`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v8.mp4',
+    description: `El gordito sabrosón se ha enfadado #tizoncito #pelusa #blacktizon #blackcat #catsofinstagram #gatonegro #iradelosmiaus #tizongorditosabroson #cats #miau #gatos #viral #parati`,
+    idPost: `${blackcats_commonProps.userId}-6d0e1d04-2dac-4f7f-8e47-4c1ecc61d614`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v9.mp4',
+    description: `Os presento al nuevo Miaucedes SUV del mercado #pelusa #tizoncito #iradelosmiaus #gato`,
+    idPost: `${blackcats_commonProps.userId}-397c28c9-7cfc-4503-a6e1-fa7b20ce0eef`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v10.mp4',
+    description: `Los planes para este fin de semana #pelusa #tizoncito #iradelosmiaus #weekends #gatosgraciosos`,
+    idPost: `${blackcats_commonProps.userId}-613257bd-2429-489a-9694-4e4e68827333`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v11.mp4',
+    description: `The dog looks stressed out with the cats presensce 🙈 . Cre livfall on TT . #cats #catlover #catlife #cats_of_instagram`,
+    idPost: `${blackcats_commonProps.userId}-276da371-8abb-41f5-ba4a-b5042786c748`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v12.mp4',
+    description: `Black cats are Orange Pro version😹 . #cats #catlover #catlife #cats_of_instagram`,
+    idPost: `${blackcats_commonProps.userId}-b467d312-cb8d-4554-bec0-590590565d77`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v13.mp4',
+    description: `She said, “ I’m not one of your TikTok lil friends!” 😅😂😂 . #cats #catlover #catlife #cats_of_instagram`,
+    idPost: `${blackcats_commonProps.userId}-a6a20e67-0771-4112-a915-d4db596daef6`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v14.mp4',
+    description: `“ we don’t speak that name in this house” 🤣 . . Cre honeyhisses on TT . #cats #catlover #catlife #cats_of_instagram`,
+    idPost: `${blackcats_commonProps.userId}-43973d48-d59b-4dc7-aaa1-c5e69ea055f7`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v15.mp4',
+    description: `#cats #catlover #catlife`,
+    idPost: `${blackcats_commonProps.userId}-9f278864-6362-4e81-a406-fc99443828a1`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v16.mp4',
+    description: `He always smells a trap😤 . #cats #catlover #catlife #cats_of_instagram
+`,
+    idPost: `${blackcats_commonProps.userId}-e8023a99-ae84-46f0-9f6f-133d9cf5b741`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v17.mp4',
+    description: `DAMN THAT IS ONE CHUNKY KITTY`,
+    idPost: `${blackcats_commonProps.userId}-53b20bf6-9713-4530-aee7-e0b96ba95280`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v18.mp4',
+    description: `Definition of chatty lovebug . #cats #catlover #catlife #cats_of_instagram`,
+    idPost: `${blackcats_commonProps.userId}-27c1569a-7950-48dc-9384-079e46448ba5`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v19.mp4',
+    description: `i’m afraid this is a nightly occurrence . #cats #catlover #catlife #cats_of_instagram`,
+    idPost: `${blackcats_commonProps.userId}-8d9b86b2-4f3c-46b3-be0c-4157fecc9ae6`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v20.mp4',
+    description: `🐈‍⬛: “anything you can do, I can do better” . #cats #catlover #catlife #cats_of_instagram`,
+    idPost: `${blackcats_commonProps.userId}-b8dee4fb-3715-4c10-a916-419d81ea91f4`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v21.mp4',
+    description: `The way theyre the same size and neither of them are small😹 . #cats #catlover #catlife #cats_of_instagram`,
+    idPost: `${blackcats_commonProps.userId}-cced4f50-6b60-4aa2-8f7e-18b0b45ee36f`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v22.mp4',
+    description: `excuse me balck pusi! . #cats #catlover #catlife #cats_of_instagram`,
+    idPost: `${blackcats_commonProps.userId}-93a0cc11-273f-4651-8ac6-255066ef8c4c`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v23.mp4',
+    description: `😂 . #cats #catlover #catlife #cats_of_instagram`,
+    idPost: `${blackcats_commonProps.userId}-7585f4bc-144f-4f63-a90b-e5c88c6fcefc`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v24.mp4',
+    description: `It always sounds like he’s saying “hooooome” . #cats #catlover #catlife #cats_of_instagram`,
+    idPost: `${blackcats_commonProps.userId}-f91a986d-94ea-4415-a3d0-e8264f71984a`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v25.mp4',
+    description: `Cat IQ test : She didn’t break the rules—she bent them. Genius. . #cats #catlover #catlife #cats_of_instagram`,
+    idPost: `${blackcats_commonProps.userId}-e1bd6ff9-073c-46a0-b0a5-2f2be98c69bb`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v26.mp4',
+    description: `The way the head just slowly appears 🤣 . #cats #catlover #catlife #cats_of_instagram`,
+    idPost: `${blackcats_commonProps.userId}-b6a3ed9e-6e59-4b2e-94d9-b182750fd0bb`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v27.mp4',
+    description: `Big boy loves to cuddle🥹 . #cats #catlover #catlife #cats_of_instagram`,
+    idPost: `${blackcats_commonProps.userId}-01bfb808-9907-4d88-8e29-6c5b340a0cd1`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v28.mp4',
+    description: `His toe beans 🥺 . #cats #catlover #catlife #cats_of_instagram`,
+    idPost: `${blackcats_commonProps.userId}-d7a7db1f-fdbb-419f-8eb6-d7d14b3c2a4d`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v29.mp4',
+    description: `Private meeting, No people allowed 😂 . #cat #catlover #cats_of_instagram #cats`,
+    idPost: `${blackcats_commonProps.userId}-20dd25bb-3e49-4b48-b5dc-7b9dce46f0ac`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v30.mp4',
+    description: `#cats_of_instagram #cats`,
+    idPost: `${blackcats_commonProps.userId}-dfce0c96-e478-422e-9287-c729ea3b63b6`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v31.mp4',
+    description: `Day 54 . . #cat #car #fyp`,
+    idPost: `${blackcats_commonProps.userId}-87d0abf6-f4cc-4a1b-bec3-8e76cc276889`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v32.mp4',
+    description: `Me when. Credit: kikvikpik #blackcats #blackcatsrule #blackcatsofig #black_cat_crew #blackcatsrock #blackcatsig #gatosnegros #blackcatsarethebest #blackcatsruletheworld #adoptblackcats #blackcatsaregoodluck #blackcatsofinsta  #blackcatsareawesome #blackcat`,
+    idPost: `${blackcats_commonProps.userId}-b7a5a2bb-3122-44c3-b4e2-b30dd0c55cfd`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v33.mp4',
+    description: `Dreaming of snacks and world domination… #BlackCatVibes #BlackCatClub #VoidBaby #NapChampion #FelineMood #CatsOfTheVoid #BlackCatsOfIG #Meowgical #CatsofInstagram #FunnyCats #ChonkyNap #BellyUpCat #CatNapsForDays`,
+    idPost: `${blackcats_commonProps.userId}-89cf6b57-26ff-4c1b-8932-6f7f2f840c61`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v34.mp4',
+    description: `Never before seen footage of Simone Biles blackcatclubs #blackcat #cats #cat #catsofinstagram #blackcatsofinstagram #catstagram #of #instacat #instagram #catlover #blackcats #catlife #meow #catlovers #blackcatsrule #kitty #kitten #adoptdontshop #c...`,
+    idPost: `${blackcats_commonProps.userId}-cd6634a0-4e52-447b-a02b-3eeb6be7ad59`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v35.mp4',
+    description: `Gym motivation. #cats #catsofinstagram #cat #meow #catoftheday #blackcatsofinstagram #catstagram #ねこ #catsgram #кот #แมว #貓 #котик #kitties #blackcatsofig #instacat #topcatphoto #catsclub #kittylove #mycat #kitty #gato #cateye #adoptdo...`,
+    idPost: `${blackcats_commonProps.userId}-b79c0c98-afe4-478b-a0f7-f308ab7f99d5`
+  },
+  {
+    type: 'video',
+    videoSrc: 'v36.mp4',
+    description: `Super BlackCat`,
+    idPost: `${blackcats_commonProps.userId}-48d78650-dc03-4994-9802-bb8d99d02737`
+  }
+];
+
+export const blackcats_array_of_posts = generateArrayPosts({
+  PREFIX,
+  ARRAY_CONTENT
+});
