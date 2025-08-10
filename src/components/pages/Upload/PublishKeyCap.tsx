8211fcf67617981b3701f6f42e77c87614f3725a
DominicDev <s0l0estudi4r@gmail.com>
2025-08-11 00:40:55 +0200

    v2.2.5

---
+++ src/components/pages/Upload/PublishKeyCap.tsx
@@ -0,0 +1,18 @@
+export function PublishKeyCap() {
+  return (
+    <article className='keycap'>
+      <aside className='letter'>
+        Publish
+        <svg fill='none' viewBox='0 0 24 24' className='enter-icon'>
+          <path
+            strokeLinejoin='round'
+            strokeLinecap='round'
+            strokeWidth='2'
+            stroke='white'
+            d='M3 12L9 18M3 12L9 6M3 12H21'
+          ></path>
+        </svg>
+      </aside>
+    </article>
+  );
+}
---
+++ src/components/pages/Upload/TextareaDescriptionPost.tsx
@@ -0,0 +1,10 @@
+export function TextareaDescriptionPost() {
+  return (
+    <aside className='container-description'>
+      <textarea
+        className='scroll-y-textarea'
+        placeholder={`Here is the description of your Post #yes #fyp #forYou`}
+      ></textarea>
+    </aside>
+  );
+}
