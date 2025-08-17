e000aba3eccffbb6fcefcd82f97b5a8227511017
DominicDev <s0l0estudi4r@gmail.com>
2025-08-17 19:02:37 +0200

    v2.9.5

---
+++ src/publicData/user-80-pala3d.ts
@@ -0,0 +1,94 @@
+import {
+  generateExactlyCommonProps,
+  generateArrayPostsOfImages,
+  generateArrayPostsOfVideos
+} from '@/publicData/functions/amPublicFunctions';
+
+const username = 'pala3d';
+const userId = '';
+const prefixUrl = '';
+const followers = 32_720;
+const followed = 322;
+const profileDescription = '3D | VFX Artist';
+const prefixLetterVideos = 'v';
+const quantityOfVideos = 9;
+const prefixLetterImages = 'n';
+const quantityOfImages = 0;
+const profileImageSrc = `${prefixUrl}p3rfil.avif`;
+
+/* prettier-ignore */
+const arrayDescriptionsOfVideos = [
+  `Roots of Code At the heart of the forest rises an immense structure intertwined with nature. It doesn’t seem man-made. It breathes like an organism, yet resembles the circuitry of an ancient machine. At its center, there’s a pure and fluid motion. Is it a message, or nature’s way of restoring itself? No one is certain. But stories say that those who pass through this blue vortex are never the same again. When nature speaks through technology, it reveals more than just a scene. It awakens something deeper. #UnrealEngine #Mountains #FantasyArt #3Dart #3DAnimation #DigitalArt #Surrealism #Art`, /*1*/
+  `Data Loom is a real-time digital system designed to behave like a weaving machine. It doesn’t play back visuals or reuse patterns. Instead, it generates color and form live, using AI-generated data as thread. Everything unfolds as you watch it. What appears on the screen is not pre-rendered. It is stitched moment by moment, driven by a collaboration between computational simulation, human intention, and machine behavior. This is not a static image. It’s a living process. `, /*2*/
+  `The Hand of the Ancients As the storm brews and lightning cracks open the sky, a lone wanderer stares at the colossal stone hand rising from the sea. No one knows who carved it or if it was ever carved at all. Some say it was left behind by gods, others believe it’s the last piece of a titan buried beneath the ocean floor. What awakens when storms gather around it remains a mystery. But one thing is certain: this is not a place you stumble upon. #unrealengine #ocean #marvel #eternal #godofwar`, /*3*/
+  `Core Remnant Between two towering mountains, hidden from satellites and untouched by time, a sphere pulses with silent energy. Nestled in a cocoon of black tendrils and faint bioluminescence, it defies classification. The researcher approaches with calculated caution, every movement recorded, every reading a step closer to truth. This is not natural, not recent. It may be a remnant of a civilization that walked this planet long before us. A weapon? A seed? A message? The truth doesn’t scream it hums, waiting to be understood. And the sphere, buried in the silence of eons, listens back. #ue5 #cave #Mountains #unrealengine #cinematic`, /*4*/
+  `Ethereal Connection I’ve been creating this kind of content at the studio I’ve worked at for years, but I had never made one to share on my own account. `, /*5*/
+  `The Forgotten Gate Carved into the heart of the earth, a monumental entrance stands in silence, its purpose long lost to time. The wind carries the echoes of an ancient past, while the shifting sands veil the secrets buried within. A threshold to another world or a warning never to cross #UE5 #Mountains #Temple #Petra`, /*6*/
+  `Awakening of the Guardian #UnrealEngine #Portal #Mystical #Mountain #hero #UE5 #3DArt #3DAnimation #DigitalArt`, /*7*/
+  `#3DArt #3DAnimation #Arrival #DigitalArt #Movie #cgi #ue5 #render #3d`, /*8*/
+  `Color Waves This artwork features a vibrant digital sculpture created using real-time capture, blending colors in a flowing, abstract #Realtime #Touchdesigner #Particles #Colors #Sculpture #Art #ArtGallery`, /*9*/ 
+  ``, /*10*/
+  ``, /*11*/
+  ``, /*12*/
+  ``, /*13*/
+  ``, /*14*/
+  ``, /*15*/
+  ``, /*16*/
+  ``, /*17*/
+  ``, /*18*/
+  ``, /*19*/
+  ``, /*20*/
+];
+
+/* prettier-ignore */
+const arrayDescriptionOfImages = [
+  ``, /*1*/
+  ``, /*2*/
+  ``, /*3*/
+  ``, /*4*/
+  ``, /*5*/
+  ``, /*6*/
+  ``, /*7*/
+  ``, /*8*/
+  ``, /*9*/
+  ``, /*10*/
+  ``, /*11*/
+  ``, /*12*/
+  ``, /*13*/
+  ``, /*14*/
+  ``, /*15*/
+  ``, /*16*/
+  ``, /*17*/
+  ``, /*18*/
+  ``, /*19*/
+  ``, /*20*/
+]
+
+export const pala3d_commonProps = generateExactlyCommonProps({
+  profileImageSrc,
+  userId,
+  username,
+  followers,
+  followed,
+  profileDescription
+});
+
+const pala3d_videos = generateArrayPostsOfVideos({
+  prefixLetterVideos,
+  prefixUrl,
+  quantityOfVideos,
+  userId,
+  arrayDescriptionsOfVideos
+});
+
+const pala3d_images = generateArrayPostsOfImages({
+  prefixLetterImages,
+  prefixUrl,
+  userId,
+  quantityOfImages,
+});
+
+export const pala3d_array_of_posts = [
+  ...pala3d_images,
+  ...pala3d_videos
+];
---
+++ src/publicData/user-81-gcodelabofficial.ts
@@ -0,0 +1,94 @@
+import {
+  generateExactlyCommonProps,
+  generateArrayPostsOfImages,
+  generateArrayPostsOfVideos
+} from '@/publicData/functions/amPublicFunctions';
+
+const username = 'gcodelabofficial';
+const userId = '';
+const prefixUrl = '';
+const followers = 111_720;
+const followed = 50;
+const profileDescription = 'Printing cool stuff in 3D!';
+const prefixLetterVideos = 'v';
+const quantityOfVideos = 9;
+const prefixLetterImages = 'n';
+const quantityOfImages = 0;
+const profileImageSrc = `${prefixUrl}p3rfil.avif`;
+
+/* prettier-ignore */
+const arrayDescriptionsOfVideos = [
+  `3D printed Shoes Printer: Bambu Lab A1 Filament: TPU 95A Blue and TPU 95A Rainbow STL file: Makerworld, Crocs Shoes and Slide Shoes by MakerVerse Designs #3dprinting #satisfying #asmr #timelapse #shoes`, /*1*/
+  `3D printed Rainbow Katana Printer`, /*2*/
+  `100k Subs 3D printed Trophy Thank you so much to all of my 100 000 subscribers here on Instagram! I really appreciate your support! STL file found here: https://makerworld.com/en/models/105003#profileId-112061 Printer: Bambu Lab A1 Filament: PLA Silk Gold + PLA Black #3dprinting #3d #shorts #reels #satisfying #timelapse #100k`, /*3*/
+  `3 Satisfying 3D Printed Swords 1/3 Collapsing Drill Sword Printer`, /*4*/
+  `3D printed Shiny Labubu Printer: Creality K2 Plus Combo + Bambu Lab A1 Filament`, /*5*/
+  `3D printed Super Mario Music Box Printer: Bambu Lab A1 Filament: PLA Matt by EXTRUDR STL file: Makerworld, Musical Fidget - Video Game Edition by kida AD: You can buy EXTRUDR filaments using my affiliate link here: https://extrudr.com/en/fi/?ref=mdlkntz use code GCODELAB for 15% OFF, code available for one time purchase in July. Link is also found through my bio or linktree.com/gcodelab #3dprinting #timelapse #nintendo #supermario #music`, /*6*/
+  `3D printed Planetary Gears Fidget Spinner Printer: Creality K2 Plus Combo Filament: PLA NX2 MATT Yellow and Light Blue by EXTRUDR STL file: https://makerworld.com/en/models/871854-razor-geometric-planetary-gears-fidget-spinner?from=search#profileId-823892 #3dprinting #fidget #fidgettoys #fidgetspinner #spinner`, /*7*/
+  `3D printed FIFA World Cup Printer: Bambu Lab A1 Filament: PLA Silk Gold & PLA Green STL file`, /*8*/
+  `3D printed Sandy the Sea Star Printer: Bambu Lab A1 Filament: PLA Silk rainbow + PLA White/Black/Green`, /*9*/ 
+  ``, /*10*/
+  ``, /*11*/
+  ``, /*12*/
+  ``, /*13*/
+  ``, /*14*/
+  ``, /*15*/
+  ``, /*16*/
+  ``, /*17*/
+  ``, /*18*/
+  ``, /*19*/
+  ``, /*20*/
+];
+
+/* prettier-ignore */
+const arrayDescriptionOfImages = [
+  ``, /*1*/
+  ``, /*2*/
+  ``, /*3*/
+  ``, /*4*/
+  ``, /*5*/
+  ``, /*6*/
+  ``, /*7*/
+  ``, /*8*/
+  ``, /*9*/
+  ``, /*10*/
+  ``, /*11*/
+  ``, /*12*/
+  ``, /*13*/
+  ``, /*14*/
+  ``, /*15*/
+  ``, /*16*/
+  ``, /*17*/
+  ``, /*18*/
+  ``, /*19*/
+  ``, /*20*/
+]
+
+export const gcodelabofficial_commonProps = generateExactlyCommonProps({
+  profileImageSrc,
+  userId,
+  username,
+  followers,
+  followed,
+  profileDescription
+});
+
+const gcodelabofficial_videos = generateArrayPostsOfVideos({
+  prefixLetterVideos,
+  prefixUrl,
+  quantityOfVideos,
+  userId,
+  arrayDescriptionsOfVideos
+});
+
+const gcodelabofficial_images = generateArrayPostsOfImages({
+  prefixLetterImages,
+  prefixUrl,
+  userId,
+  quantityOfImages,
+});
+
+export const gcodelabofficial_array_of_posts = [
+  ...gcodelabofficial_images,
+  ...gcodelabofficial_videos
+];
