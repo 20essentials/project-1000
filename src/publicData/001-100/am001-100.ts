261cc7c681da9ca1c1466db27adf848f2405d1c8
DominicDev <s0l0estudi4r@gmail.com>
2025-08-22 16:21:24 +0200

    v3.2.2

---
+++ src/publicData/001-100/am001-100.ts
@@ -0,0 +1,413 @@
+import type { arrayOfPosts } from '@/components/pages/SliderPosts/types.d.ts';
+import {
+  goated_ai_art_array_of_posts,
+  goated_ai_art_commonProps
+} from './user-2-goated-ai-art';
+import {
+  edaserkanbolat_edits_array_of_posts,
+  edaserkanbolat_edits_commonProps
+} from './user-11-edaserkanbolat-edits';
+import {
+  jorgepomarg_array_of_posts,
+  jorgepomarg_commonProps
+} from './user-12-jorgepomarg';
+import {
+  lucia_sora_array_of_posts,
+  lucia_sora_commonProps
+} from './user-13-luciasora';
+import {
+  elcodigobravo_array_of_posts,
+  elcodigobravo_commonProps
+} from './user-14-elcodigo-bravo';
+import {
+  mr_rombus_array_of_posts,
+  mr_rombus_commonProps
+} from './user-15-mr-rombus';
+import { cata_array_of_posts, cata_commonProps } from './user-16-cata';
+import {
+  whoisangenr_array_of_posts,
+  whoisangenr_commonProps
+} from './user-17-whoisangenr';
+import {
+  pumpkim_array_of_posts,
+  pumpkim_commonProps
+} from './user-18-pumpkim';
+import {
+  gregorian_array_of_posts,
+  gregorian_commonProps
+} from './user-19-gregorian';
+import {
+  milan_cars_array_of_posts,
+  milan_cars_commonProps
+} from './user-20-milan-cars';
+import {
+  francesco_viaggi_array_of_posts,
+  francesco_viaggi_commonProps
+} from './user-21-francesco-viaggi';
+import {
+  ariel_array_of_posts,
+  ariel_commonProps
+} from './user-22-ariel';
+import {
+  javier_santaolalla_array_of_posts,
+  javier_santaolalla_commonProps
+} from './user-23-javier-santaolalla';
+import {
+  mr_robot_array_of_posts,
+  mr_robot_commonProps
+} from './user-24-mr-robot';
+import {
+  mr_benz_array_of_posts,
+  mr_benz_commonProps
+} from './user-25-mr-benz';
+import {
+  about_life_array_of_posts,
+  about_life_commonProps
+} from './user-27-about-life';
+import {
+  arte_settima_array_of_posts,
+  arte_settima_commonProps
+} from './user-26-arte-settima';
+import {
+  pongamoslo_a_prueba_array_of_posts,
+  pongamoslo_a_prueba_commonProps
+} from './user-28-pongamoslo-a-prueba';
+import {
+  the_simulator_array_of_posts,
+  the_simulator_commonProps
+} from './user-29-the-simulator';
+import {
+  lacosaes_array_of_posts,
+  lacosaes_commonProps
+} from './user-30-lacosaes';
+import {
+  miguel_araiza_array_of_posts,
+  miguel_araiza_commonProps
+} from './user-31-miguel-araiza';
+import {
+  presocratico_array_of_posts,
+  presocratico_commonProps
+} from './user-32-presocratico';
+import {
+  eddy_beef_array_of_posts,
+  eddy_beef_commonProps
+} from './user-33-eddy-beef';
+import {
+  lalobri_array_of_posts,
+  lalobri_commonProps
+} from './user-34-lalobri';
+import {
+  soy_delishus_array_of_posts,
+  soy_delishus_commonProps
+} from './user-35-soydelishus';
+import {
+  arthur_array_of_posts,
+  arthur_commonProps
+} from './user-36-arthur';
+import {
+  ellian_sanchez_array_of_posts,
+  ellian_sanchez_commonProps
+} from './user-37-ellian-sanchez';
+import {
+  salary_transparent_street_array_of_posts,
+  salary_transparent_street_commonProps
+} from './user-38-salary-transparent-street';
+import {
+  gianco123_array_of_posts,
+  gianco123_commonProps
+} from './user-39-gianco123';
+import {
+  laVidaDeTomas_array_of_posts,
+  laVidaDeTomas_commonProps
+} from './user-41-la-vida-de-tomas';
+import {
+  holvek_array_of_posts,
+  holvek_commonProps
+} from './user-40-holvek';
+import {
+  geridoo_array_of_posts,
+  geridoo_commonProps
+} from './user-42-geridoo';
+import {
+  riccardosegna_array_of_posts,
+  riccardosegna_commonProps
+} from './user-43-ricardos3gna';
+import {
+  manuel_villa_array_of_posts,
+  manuel_villa_commonProps
+} from './user-44-manuel-villa';
+import {
+  chico_collection_array_of_posts,
+  chico_collection_commonProps
+} from './user-45-chico-collection';
+import {
+  justtrip_array_of_posts,
+  justtrip_commonProps
+} from './user-46-justtrip';
+import {
+  mycelium_mirrors_array_of_posts,
+  mycelium_mirrors_commonProps
+} from './user-47-mycelium-mirrors';
+import {
+  blackcats_array_of_posts,
+  blackcats_commonProps
+} from './user-49-blackcats';
+import {
+  ligthspeed_array_of_posts,
+  ligthspeed_commonProps
+} from './user-48-ligth-speed';
+import { beitman_array_of_posts, beitman_commonProps } from './user-51-beitman';
+import {
+  ghxz_edits_array_of_posts,
+  ghxz_edits_commonProps
+} from './user-50-ghxz-edit';
+import {
+  gafurovedit_array_of_posts,
+  gafurovedit_commonProps
+} from './user-52-gafurovedit';
+import { improve_array_of_posts, improve_commonProps } from './user-53-improve';
+import {
+  dg_minisetoys_array_of_posts,
+  dg_minisetoys_commonProps
+} from './user-54-dg-minisetoys';
+import {
+  jiraiya_fanboy_array_of_posts,
+  jiraiya_fanboy_commonProps
+} from './user-55-jiraiya-fanboy';
+import {
+  eric_sierra_array_of_posts,
+  eric_sierra_commonProps
+} from './user-57-eric-sierra';
+import {
+  ximu_cat_array_of_posts,
+  ximu_cat_commonProps
+} from './user-56-ximu-cat';
+import {
+  turnsignalthecat_array_of_posts,
+  turnsignalthecat_commonProps
+} from './user-61-turnsignalthecat';
+import { lggbb_array_of_posts, lggbb_commonProps } from './user-58-lggbb';
+import {
+  dreamfall_array_of_posts,
+  dreamfall_commonProps
+} from './user-62-dreamfall';
+import {
+  mr_rizzer_array_of_posts,
+  mr_rizzer_commonProps
+} from './user-59-mr-rizzer';
+import {
+  soyveyrox_array_of_posts,
+  soyveyrox_commonProps
+} from './user-60-soyveyrox';
+import {
+  freeskates_array_of_posts,
+  freeskates_commonProps
+} from './user-63-freeskates';
+import {
+  gian_piccin_array_of_posts,
+  gian_piccin_commonProps
+} from './user-64-gian-piccin';
+import {
+  aiaartsensei_array_of_posts,
+  aiaartsensei_commonProps
+} from './user-65-aiartsensei';
+import {
+  mateomassoni_array_of_posts,
+  mateomassoni_commonProps
+} from './user-66-mateomassoni';
+import {
+  spirograph_array_of_posts,
+  spirograph_commonProps
+} from './user-68-spirograph';
+import {
+  magi_skill_array_of_posts,
+  magi_skill_commonProps
+} from './user-5-magiskill';
+import {
+  molly_zombie_array_of_posts,
+  molly_zombie_commonProps
+} from './user-67-mollyzombie';
+import { midudev_array_of_posts, midudev_commonProps } from './user-1-midudev';
+import {
+  arsnotoria_array_of_posts,
+  arsnotoria_commonProps
+} from './user-3-arsnotoria';
+import {
+  mr_lamborghini_array_of_posts,
+  mr_lamborghini_commonProps
+} from './user-4-mrlamborghini';
+import { qbitlab_array_of_posts, qbitlab_commonProps } from './user-6-qbitl4b';
+import {
+  vid_favorites_array_of_posts,
+  vid_favorites_commonProps
+} from './user-7-vidsfavorites';
+import {
+  siti_favolosi_array_of_posts,
+  siti_favolosi_commonProps
+} from './user-8-sitifavolosi';
+import { luxury_array_of_posts, luxury_commonProps } from './user-9-theluxury';
+import {
+  marshall_jassan_array_of_posts,
+  marshall_jassan_commonProps
+} from './user-10-marshall';
+import {
+  cagnolino_array_of_posts,
+  cagnolino_commonProps
+} from './user-70-cagnolino';
+import { juixxe_array_of_posts, juixxe_commonProps } from './user-69-juixxe';
+import {
+  predmet_ia_array_of_posts,
+  predmet_ia_commonProps
+} from './user-72-predmet';
+import {
+  slinky_josh_array_of_posts,
+  slinky_josh_commonProps
+} from './user-71-slinkyjosh';
+import { mr_spary_array_of_posts, mr_spary_commonProps } from './user-73-mrspary';
+import {
+  nagy_visuals_array_of_posts,
+  nagy_visuals_commonProps
+} from './user-74-nagy-visuals';
+import {
+  rocodelaportilla_array_of_posts,
+  rocodelaportilla_commonProps
+} from './user-78-rocodelaportilla';
+import {
+  gazzaladradesign_array_of_posts,
+  gazzaladradesign_commonProps
+} from './user-76-gazzaladradesign';
+import {
+  morpheussosia_array_of_posts,
+  morpheussosia_commonProps
+} from './user-75-morpheussosia';
+import {
+  johan_wahlgren_array_of_posts,
+  johan_wahlgren_commonProps
+} from './user-77-johan-wahlgren';
+import {
+  polymico_array_of_posts,
+  polymico_commonProps
+} from './user-79-polymico';
+import { pala3d_array_of_posts, pala3d_commonProps } from './user-80-pala3d';
+import {
+  gcodelabofficial_array_of_posts,
+  gcodelabofficial_commonProps
+} from './user-81-gcodelabofficial';
+import {
+  ok_toaster_array_of_posts,
+  ok_toaster_commonProps
+} from './user-83-ok-toaster';
+import {
+  creation_ross_array_of_posts,
+  creation_ross_commonProps
+} from './user-82-creationross';
+import { aaronblaiseart_array_of_posts, aaronblaiseart_commonProps } from './user-87-aaronblaiseart';
+import { kengam_array_of_posts, kengam_commonProps } from './user-84-kengam';
+import { roller_bearding_array_of_posts, roller_bearding_commonProps } from './user-86-rollerbearding';
+import { ringwitdahoodie_array_of_posts, ringwitdahoodie_commonProps } from './user-88-ringwitdahoodie';
+import { andrey_sorokyn_array_of_posts, andrey_sorokyn_commonProps } from './user-85-andrey-sorokyn';
+import { meerkat_array_of_posts, meerkat_commonProps } from './user-90-meerkat';
+import { raccoons_array_of_posts, raccoons_commonProps } from './user-89-raccoons';
+import { jsmedellin_array_of_posts, jsmedellin_commonProps } from './user-91-jsmedellin';
+import { childhood_2000s_array_of_posts, childhood_2000s_commonProps } from './user-92-childhood-2000';
+
+export const PUBLIC_001_100: arrayOfPosts = [
+  [midudev_commonProps, midudev_array_of_posts] /* 1 */,
+  [goated_ai_art_commonProps, goated_ai_art_array_of_posts] /* 2 */,
+  [arsnotoria_commonProps, arsnotoria_array_of_posts] /* 3 */,
+  [mr_lamborghini_commonProps, mr_lamborghini_array_of_posts] /* 4 */,
+  [magi_skill_commonProps, magi_skill_array_of_posts] /* 5 */,
+  [qbitlab_commonProps, qbitlab_array_of_posts] /* 6 */,
+  [vid_favorites_commonProps, vid_favorites_array_of_posts] /* 7 */,
+  [siti_favolosi_commonProps, siti_favolosi_array_of_posts] /* 8*/,
+  [luxury_commonProps, luxury_array_of_posts] /* 9 */,
+  [marshall_jassan_commonProps, marshall_jassan_array_of_posts] /* 10 */,
+  [
+    edaserkanbolat_edits_commonProps,
+    edaserkanbolat_edits_array_of_posts
+  ] /* 11 */,
+  [jorgepomarg_commonProps, jorgepomarg_array_of_posts] /* 12 */,
+  [lucia_sora_commonProps, lucia_sora_array_of_posts] /* 13 */,
+  [elcodigobravo_commonProps, elcodigobravo_array_of_posts] /* 14*/,
+  [mr_rombus_commonProps, mr_rombus_array_of_posts] /* 15*/,
+  [cata_commonProps, cata_array_of_posts] /* 16*/,
+  [whoisangenr_commonProps, whoisangenr_array_of_posts] /* 17 */,
+  [pumpkim_commonProps, pumpkim_array_of_posts] /* 18 */,
+  [gregorian_commonProps, gregorian_array_of_posts] /* 19 */,
+  [milan_cars_commonProps, milan_cars_array_of_posts] /* 20 */,
+  [francesco_viaggi_commonProps, francesco_viaggi_array_of_posts] /* 21 */,
+  [ariel_commonProps, ariel_array_of_posts] /* 22 */,
+  [javier_santaolalla_commonProps, javier_santaolalla_array_of_posts] /* 23 */,
+  [mr_robot_commonProps, mr_robot_array_of_posts] /* 24 */,
+  [mr_benz_commonProps, mr_benz_array_of_posts] /* 25 */,
+  [arte_settima_commonProps, arte_settima_array_of_posts] /* 26 */,
+  [about_life_commonProps, about_life_array_of_posts] /* 27 */,
+  [pongamoslo_a_prueba_commonProps, pongamoslo_a_prueba_array_of_posts],
+  [the_simulator_commonProps, the_simulator_array_of_posts] /* 29 */,
+  [lacosaes_commonProps, lacosaes_array_of_posts] /* 30 */,
+  [miguel_araiza_commonProps, miguel_araiza_array_of_posts] /* 31 */,
+  [presocratico_commonProps, presocratico_array_of_posts] /* 32 */,
+  [eddy_beef_commonProps, eddy_beef_array_of_posts] /* 33 */,
+  [lalobri_commonProps, lalobri_array_of_posts] /* 34 */,
+  [soy_delishus_commonProps, soy_delishus_array_of_posts] /* 35 */,
+  [arthur_commonProps, arthur_array_of_posts] /* 36 */,
+  [ellian_sanchez_commonProps, ellian_sanchez_array_of_posts] /* 37 */,
+  [
+    salary_transparent_street_commonProps,
+    salary_transparent_street_array_of_posts
+  ] /* 38 */,
+  [gianco123_commonProps, gianco123_array_of_posts] /* 39 */,
+  [holvek_commonProps, holvek_array_of_posts] /* 40 */,
+  [laVidaDeTomas_commonProps, laVidaDeTomas_array_of_posts] /* 41 */,
+  [geridoo_commonProps, geridoo_array_of_posts] /* 42 */,
+  [riccardosegna_commonProps, riccardosegna_array_of_posts] /* 43 */,
+  [manuel_villa_commonProps, manuel_villa_array_of_posts] /* 44 */,
+  [chico_collection_commonProps, chico_collection_array_of_posts] /* 45 */,
+  [justtrip_commonProps, justtrip_array_of_posts] /* 46 */,
+  [mycelium_mirrors_commonProps, mycelium_mirrors_array_of_posts] /* 47 */,
+  [ligthspeed_commonProps, ligthspeed_array_of_posts] /* 48 */,
+  [blackcats_commonProps, blackcats_array_of_posts] /* 49 */,
+  [ghxz_edits_commonProps, ghxz_edits_array_of_posts] /* 50 */,
+  [beitman_commonProps, beitman_array_of_posts] /* 51 */,
+  [gafurovedit_commonProps, gafurovedit_array_of_posts] /* 52 */,
+  [improve_commonProps, improve_array_of_posts] /* 53 */,
+  [dg_minisetoys_commonProps, dg_minisetoys_array_of_posts] /* 54 */,
+  [jiraiya_fanboy_commonProps, jiraiya_fanboy_array_of_posts] /* 55 */,
+  [ximu_cat_commonProps, ximu_cat_array_of_posts] /* 56 */,
+  [eric_sierra_commonProps, eric_sierra_array_of_posts] /* 57 */,
+  [lggbb_commonProps, lggbb_array_of_posts] /* 58 */,
+  [mr_rizzer_commonProps, mr_rizzer_array_of_posts] /* 59 */,
+  [soyveyrox_commonProps, soyveyrox_array_of_posts] /* 60 */,
+  [turnsignalthecat_commonProps, turnsignalthecat_array_of_posts] /* 61 */,
+  [dreamfall_commonProps, dreamfall_array_of_posts] /* 62 */,
+  [freeskates_commonProps, freeskates_array_of_posts] /* 63 */,
+  [gian_piccin_commonProps, gian_piccin_array_of_posts] /* 64 */,
+  [aiaartsensei_commonProps, aiaartsensei_array_of_posts] /* 65 */,
+  [mateomassoni_commonProps, mateomassoni_array_of_posts] /* 66 */,
+  [molly_zombie_commonProps, molly_zombie_array_of_posts] /* 67 */,
+  [spirograph_commonProps, spirograph_array_of_posts] /* 68 */,
+  [juixxe_commonProps, juixxe_array_of_posts] /* 69 */,
+  [cagnolino_commonProps, cagnolino_array_of_posts] /* 70 */,
+  [slinky_josh_commonProps, slinky_josh_array_of_posts] /* 71 */,
+  [predmet_ia_commonProps, predmet_ia_array_of_posts] /* 72 */,
+  [mr_spary_commonProps, mr_spary_array_of_posts] /* 73 */,
+  [nagy_visuals_commonProps, nagy_visuals_array_of_posts] /* 74 */,
+  [morpheussosia_commonProps, morpheussosia_array_of_posts] /* 75 */,
+  [gazzaladradesign_commonProps, gazzaladradesign_array_of_posts] /* 76 */,
+  [johan_wahlgren_commonProps, johan_wahlgren_array_of_posts] /* 77 */,
+  [rocodelaportilla_commonProps, rocodelaportilla_array_of_posts] /* 78 */,
+  [polymico_commonProps, polymico_array_of_posts] /* 79 */,
+  [pala3d_commonProps, pala3d_array_of_posts] /* 80 */,
+  [gcodelabofficial_commonProps, gcodelabofficial_array_of_posts] /* 81 */,
+  [creation_ross_commonProps, creation_ross_array_of_posts] /* 82 */,
+  [ok_toaster_commonProps, ok_toaster_array_of_posts], /* 83 */
+  [kengam_commonProps, kengam_array_of_posts], /* 84 */
+  [andrey_sorokyn_commonProps, andrey_sorokyn_array_of_posts], /* 85 */
+  [roller_bearding_commonProps, roller_bearding_array_of_posts], /* 86 */
+  [aaronblaiseart_commonProps, aaronblaiseart_array_of_posts], /* 87 */
+  [ringwitdahoodie_commonProps, ringwitdahoodie_array_of_posts], /* 88 */
+  [raccoons_commonProps,raccoons_array_of_posts], /* 89 */
+  [meerkat_commonProps, meerkat_array_of_posts], /* 90 */
+  [jsmedellin_commonProps, jsmedellin_array_of_posts], /* 91 */
+  [childhood_2000s_commonProps, childhood_2000s_array_of_posts], /* 92 */
+];
