881e67747adc9312a1aebdce2d05ae4a7c5445aa
DominicDev <s0l0estudi4r@gmail.com>
2025-08-26 21:04:08 +0200

    v3.5.3

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,11 @@ import { getViteConfig } from 'astro/config';
 
 export default getViteConfig({
   test: {
+    include: [
+      'src/tests/unit/**/*.{test,spec}.{js,jsx,ts,tsx}',
+      'src/tests/integration/**/*.{test,spec}.{js,jsx,ts,tsx}'
+    ],
+    exclude: ['src/tests/e2e/**/*'],
     globals: true,
     environment: 'happy-dom'
   }
