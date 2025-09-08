1da62d1b0411443bfb484bc68e368373344ea87b
DominicDev <s0l0estudi4r@gmail.com>
2025-09-09 00:22:37 +0200

    v4.6.3

--- src/tests/e2e/e2eUtils.ts
+++ src/tests/e2e/e2eUtils.ts
@@ -1,8 +1,28 @@
-export const userId = '1339e4cd-cd20-40b5-8e47-1f4483d586a3';
-export const postId =
-  '1339e4cd-cd20-40b5-8e47-1f4483d586a3-img-f95fbbd1-4c2f-4575-8699-c4dff4e0d0b9000035';
-export const postUrlFromUserTest = `/?userId=${userId}&postId=${postId}`;
-export const homeURL = '/'
+export const USER_TEST = {
+  username: 'midudev',
+  userId: '21a87f69-c341-492f-a156-47588fd606dd6',
+  prefixUrl: 'https://dominicnikolai.github.io/project-208/assets/m1dudev/',
+  followers: 8720322,
+  followed: 122,
+  profileDescription:
+    '👨‍💻 Programación Web con JavaScript Cursos, Tutoriales, Trucos y Consejos',
+  profileImageSrc:
+    'https://dominicnikolai.github.io/project-208/assets/m1dudev/p3rfil.avif'
+};
+
+export const POST_1_USER_TEST = {
+  type: 'video',
+  videoSrc: 'v1.mp4',
+  description: `¿Te cuesta el CSS y el diseño de tu web?
+¡Este recurso te va a facilitar la vida!
+Crea layouts con Grid de forma visual e interactiva.
+Es personalizable y luego te da todo el código que necesitas para tu página.
+¡Deja tu corazón si te sirve! #software #programacion #uxui #css3 #webdev`,
+  idPost: `${USER_TEST.userId}-9fd97a84-c39d-4c2e-872f-53eb5fbdfe4e`
+};
+
+export const postUrlFromUserTest = `/?userId=${USER_TEST.userId}&postId=${POST_1_USER_TEST.idPost}`;
+export const homeURL = '/';
 export const searchQueryValue = 'hello';
 export const searchURL = `/?search=${searchQueryValue}`;
 export const uploadURL = '/?upload=view';
--- src/tests/e2e/home/homeQueryParams.spec.ts
+++ src/tests/e2e/home/homeQueryParams.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 import {
   postUrlFromUserTest,
-  userId,
-  postId,
+  POST_1_USER_TEST,
+  USER_TEST,
   queryParam
 } from '@/tests/e2e/e2eUtils';
 
@@ -11,8 +11,8 @@ test('the userId and postId params are consistent', async ({ page }) => {
   await expect(page).toHaveURL(url => {
     const searchParams = new URL(url).searchParams;
     return (
-      searchParams.get(queryParam.userId) === userId &&
-      searchParams.get(queryParam.postId) === postId
+      searchParams.get(queryParam.userId) === USER_TEST.userId &&
+      searchParams.get(queryParam.postId) === POST_1_USER_TEST.idPost
     );
   });
 });
